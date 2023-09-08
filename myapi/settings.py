2806972687166b63e35bf973a4d908ef4886b9e9
김소망 <133929501+Vitifolia@users.noreply.github.com>
2023-09-08 23:47:58 +0900

    6

--- myapi/settings.py
+++ myapi/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(default='sqlite:///db.sqlite3')
 DATABASES['default'].update(db_from_env)
 
 
