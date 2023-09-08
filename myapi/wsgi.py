d3a108196e2b353f595c7ffc276de677619bf5bf
김소망 <133929501+Vitifolia@users.noreply.github.com>
2023-09-08 14:41:33 +0900

    5

--- myapi/wsgi.py
+++ myapi/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapi.settings')
 
 application = get_wsgi_application()
+
+
+# koyeb app init myapi \
+#   --git github.com/Vitifolia/quiz-test \
+#   --git-branch main \
+#   --git-run-command "gunicorn myapi.wsgi" \
+#   --ports 8000:http \
+#   --routes /:8000 \
+#   --env PORT=8000 \
+#   --env DJANGO_ALLOWED_HOSTS=myapi-vitifolia.koyeb.app
